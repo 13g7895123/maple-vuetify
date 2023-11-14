c84cb8839ea0c9df0333bbc8583961d5487fca0d
Jarvis <13g7895123@gmail.com>
2023-11-15 00:03:02 +0800

    [edit] revise img render

--- vue-app/src/views/Login.vue
+++ vue-app/src/views/Login.vue
@@ -11,7 +11,7 @@
             >
                 <v-img
                   cover
-                  :height="50"
+                  :height="10"
                   src="../src/assets/login_img.png"
                 ></v-img>
             </v-sheet>
