9ef792742f00f10de6744a792257cad01151b5e1
Jarvis <13g7895123@gmail.com>
2023-11-14 21:29:39 +0800

    [edit] add login bg

--- vue-app/tailwind.config.js
+++ vue-app/tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: 'jit',
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'loginBg': 'url("./src/assets/christmas_1920_1200.jpg")'
+      }
+    },
   },
   plugins: [],
 }
