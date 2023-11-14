0958b05caa4166d36a82f7906bb56b157eb88f43
Jarvis <13g7895123@gmail.com>
2023-11-14 21:41:16 +0800

    [edit] fix bg no render

--- vue-app/src/views/Login.vue
+++ vue-app/src/views/Login.vue
@@ -1,7 +1,12 @@
 <template>
     <v-app>
-        <v-main class="mt-4 bg-loginBg bg-cover bg-no-repeat bg-center">
+        <v-main class="bg-loginBg bg-cover bg-no-repeat bg-center" id="loginBg">
             
         </v-main>
     </v-app>
 </template>
+<style>
+#loginBg{
+    background-image: url('../assets/christmas_1920_1200.jpg');
+}
+</style>
