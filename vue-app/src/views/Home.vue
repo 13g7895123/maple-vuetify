3c042dbfda02af15d4473d4f80f56b5124731456
Jarvis <13g7895123@gmail.com>
2023-11-28 08:14:32 +0800

    [edit] test render

--- vue-app/src/views/Home.vue
+++ vue-app/src/views/Home.vue
@@ -5,12 +5,10 @@
         <Sidebar />
         <!-- <Navbar /> -->
         <v-main>
-            <v-container class="mx-3 mt-2" fluid style="background-color: #555;">
-                <v-row>
-                    <v-col cols="12" sm="4">
-                        <router-view />
-                    </v-col>
-                </v-row>
+            <v-container class="px-3 mt-2" fluid>
+                <div class="mx-3" style="background-color: #555;">
+                    <router-view />
+                </div>
             </v-container>
         </v-main>
     </v-app>
