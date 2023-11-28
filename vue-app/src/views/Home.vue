8d22a04b256ba4d14c58c77220c7d0db190670c0
Jarvis <13g7895123@gmail.com>
2023-11-28 08:13:26 +0800

    [edit] test render

--- vue-app/src/views/Home.vue
+++ vue-app/src/views/Home.vue
@@ -6,8 +6,11 @@
         <!-- <Navbar /> -->
         <v-main>
             <v-container class="mx-3 mt-2" fluid style="background-color: #555;">
-                <!-- <v-col cols="12" sm="4"></v-col> -->
-                <router-view />
+                <v-row>
+                    <v-col cols="12" sm="4">
+                        <router-view />
+                    </v-col>
+                </v-row>
             </v-container>
         </v-main>
     </v-app>
