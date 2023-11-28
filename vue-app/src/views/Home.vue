08682d97d4ebe561733bd38d38cd9a0de88c26c5
Jarvis <13g7895123@gmail.com>
2023-11-28 08:11:39 +0800

    [edit] test render

--- vue-app/src/views/Home.vue
+++ vue-app/src/views/Home.vue
@@ -5,7 +5,7 @@
         <Sidebar />
         <!-- <Navbar /> -->
         <v-main>
-            <v-container class="px-6 mt-n2" fluid style="background-color: #555;">
+            <v-container class="mx-6 mt-2" fluid style="background-color: #555;">
                 <!-- <v-col cols="12" sm="4"></v-col> -->
                 <router-view />
             </v-container>
