95f13c6013f9f0f904c32e9d592940d635e76053
Jarvis <13g7895123@gmail.com>
2023-12-24 01:18:57 +0800

    [edit] change color

--- vue-app/src/views/FrenzyTotem.vue
+++ vue-app/src/views/FrenzyTotem.vue
@@ -2,7 +2,7 @@
     <v-container fluid>
         <v-row>
             <v-col cols="12" sm="4">
-                <v-card colors="#B49239" class="rounded-xl" height="150">
+                <v-card color="#B49239" class="rounded-xl" height="150">
                     <div class="d-flex flex-no-wrap justify-space-between">
                         <div>
                             <v-card-title>當日累績(金額/時數)</v-card-title>
