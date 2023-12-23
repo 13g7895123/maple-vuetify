5c2d1e1626ccd09995ee9c5b5135a3258851cb63
Jarvis <13g7895123@gmail.com>
2023-12-24 01:12:45 +0800

    [edit] render content

--- vue-app/src/views/FrenzyTotem.vue
+++ vue-app/src/views/FrenzyTotem.vue
@@ -1,3 +1,11 @@
 <template>
-    <label>test123</label>
+    <v-container fluid>
+        <v-row>
+            <v-col cols="12" sm="4">
+                <v-card colors="#B49239" theme="dark" class="rounded-xl" height="150">
+                    
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
