3ac38d39f19439192029845d917fd39d2eabb867
Jarvis <13g7895123@gmail.com>
2023-12-24 01:36:41 +0800

    [edit] revise render

--- vue-app/src/views/FrenzyTotem.vue
+++ vue-app/src/views/FrenzyTotem.vue
@@ -13,8 +13,8 @@
             </v-col>
             <v-col cols="12" sm="4">
                 <v-card color="#B49239" class="rounded-xl" height="150">
-                    <div class="h-full flex flex-nowrap justify-space-between">
-                        <div>
+                    <div class="h-full flex flex-nowrap justify-between">
+                        <div class="flex flex-col items-center">
                             <v-card-title>當月累績(金額/時數)</v-card-title>
                             <v-card-subtitle>6e/6H</v-card-subtitle>
                         </div>
