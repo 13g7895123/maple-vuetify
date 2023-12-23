1e0deb965f3d5d3e114559a8821ba19ce559a8ff
Jarvis <13g7895123@gmail.com>
2023-12-24 01:20:49 +0800

    [edit] change color

--- vue-app/src/views/FrenzyTotem.vue
+++ vue-app/src/views/FrenzyTotem.vue
@@ -11,6 +11,26 @@
                     </div>
                 </v-card>
             </v-col>
+            <v-col cols="12" sm="4">
+                <v-card color="#B49239" class="rounded-xl" height="150">
+                    <div class="d-flex flex-no-wrap justify-space-between">
+                        <div>
+                            <v-card-title>當月累績(金額/時數)</v-card-title>
+                            <v-card-subtitle>6e/6H</v-card-subtitle>
+                        </div>
+                    </div>
+                </v-card>
+            </v-col>
+            <v-col cols="12" sm="4">
+                <v-card color="#B49239" class="rounded-xl" height="150">
+                    <div class="d-flex flex-no-wrap justify-space-between">
+                        <div>
+                            <v-card-title>過去累績(金額/時數)</v-card-title>
+                            <v-card-subtitle>9e/9H</v-card-subtitle>
+                        </div>
+                    </div>
+                </v-card>
+            </v-col>
         </v-row>
     </v-container>
 </template>
