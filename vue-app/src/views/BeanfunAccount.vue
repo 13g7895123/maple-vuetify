3f0503ee6b75ee622306d79a211d7295d77b7bde
Jarvis <13g7895123@gmail.com>
2024-03-02 18:19:35 +0800

    revise form

--- vue-app/src/views/BeanfunAccount.vue
+++ vue-app/src/views/BeanfunAccount.vue
@@ -1,52 +1,48 @@
 <template>
     <v-app>
         <v-main id="login_main">
-            <v-layout align-center align-content-center>
-
-            
-                <v-card
-                title="Beanfun Account"
-                :elevation="6" 
-                :width="400"
-                :height="540"
-                rounded
-                style="background:rgba(255, 255, 255, 0.9);"
+            <v-card
+            title="Beanfun Account"
+            :elevation="6" 
+            :width="400"
+            :height="540"
+            rounded
+            style="background:rgba(255, 255, 255, 0.9);"
+            >
+                <v-form 
+                @submit.prevent
+                class="d-flex flex-column align-center justify-center"
                 >
-                    <v-form 
-                    @submit.prevent
-                    class="d-flex flex-column align-center justify-center"
-                    >
-                        <v-text-field
-                            variant="outlined"
-                            label="Account"
-                            height="30px"
-                            class="mt-3"
-                            style="width: 70%;"
-                        ></v-text-field>
-                        <v-text-field
-                            variant="outlined"
-                            label="email"
-                            style="width: 70%;"
-                        ></v-text-field>
-                        <v-text-field
-                            variant="outlined"
-                            label="phone"
-                            style="width: 70%;"
-                        ></v-text-field>
-                        <v-text-field
-                            variant="outlined"
-                            label="phone owner"
-                            style="width: 70%;"
-                        ></v-text-field>
-                        <v-btn type="button" 
-                            class="mt-2"
-                            style="width: 70%; color:#fff;"
-                            color="#4287f5"
-                            @click="router.push('/bonus')"
-                        >ENTER</v-btn>
-                    </v-form>                
-                </v-card>
-            </v-layout>
+                    <v-text-field
+                        variant="outlined"
+                        label="Account"
+                        height="30px"
+                        class="mt-3"
+                        style="width: 70%;"
+                    ></v-text-field>
+                    <v-text-field
+                        variant="outlined"
+                        label="email"
+                        style="width: 70%;"
+                    ></v-text-field>
+                    <v-text-field
+                        variant="outlined"
+                        label="phone"
+                        style="width: 70%;"
+                    ></v-text-field>
+                    <v-text-field
+                        variant="outlined"
+                        label="phone owner"
+                        style="width: 70%;"
+                    ></v-text-field>
+                    <v-btn type="button" 
+                        class="mt-2"
+                        style="width: 70%; color:#fff;"
+                        color="#4287f5"
+                        @click="router.push('/bonus')"
+                    >ENTER</v-btn>
+                </v-form>                
+            </v-card>
         </v-main>
     </v-app>
 </template>
