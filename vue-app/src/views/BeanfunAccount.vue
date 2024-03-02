89e11bb4acfd014902802e9d011dc78f93cb7ad0
Jarvis <13g7895123@gmail.com>
2024-03-02 18:26:10 +0800

    revise form

--- vue-app/src/views/BeanfunAccount.vue
+++ vue-app/src/views/BeanfunAccount.vue
@@ -8,7 +8,7 @@
             :height="540"
             rounded
             style="background:rgba(255, 255, 255, 0.9);"     
-            class="d-flex flex-column align-center"       
+            class="d-flex flex-column justify-center"       
             >
                 <v-form 
                 @submit.prevent
