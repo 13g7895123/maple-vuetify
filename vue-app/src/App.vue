2dcd0206fc90a8787aa9503028c3ef6a7021268c
Jarvis <13g7895123@gmail.com>
2024-03-02 17:46:57 +0800

    fix page empty

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <v-app>
     <v-main>
-      <Suspense>
+      <!-- <Suspense> -->
         <router-view />
-      </Suspense>
+      <!-- </Suspense> -->
     </v-main>
   </v-app>
 </template>
