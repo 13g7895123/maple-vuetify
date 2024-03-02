880beead7d6c2147ba654c7b851ddee93afe91e7
Jarvis <13g7895123@gmail.com>
2024-03-02 21:35:33 +0800

    fix function error

--- vue-app/src/views/MapleAccount.vue
+++ vue-app/src/views/MapleAccount.vue
@@ -110,7 +110,7 @@ if (testMode == 1){
 onMounted(async() => {
     const { data: { success, data } }  = await axios.get('http://170.187.229.132:9092/api/beanfun');
     if (success){
-        for (let i = 0; i < length(data); i++){
+        for (let i = 0; i < data.length; i++){
             bfAccountList.value.push(data[i]['account'])
         }
     }
