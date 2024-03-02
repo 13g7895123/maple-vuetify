85f68c0f9100dd4348000cf0152d3b2d0cc0c19f
Jarvis <13g7895123@gmail.com>
2024-03-02 19:03:46 +0800

    revise form

--- vue-app/src/views/BeanfunAccount.vue
+++ vue-app/src/views/BeanfunAccount.vue
@@ -7,7 +7,8 @@
             :width="400"
             :height="540"
             rounded
-            style="background:rgba(255, 255, 255, 0.9);"         
+            style="background:rgba(255, 255, 255, 0.9);"     
+            class="d-flex flex-column justify-space-around align-center"       
             >
                 <v-form 
                 @submit.prevent
@@ -17,23 +18,26 @@
                         variant="outlined"
                         label="Account"
                         height="30px"
-                        width="auto"
+                        width="280px"
                         class="mt-3"
                         style="width: 70%;"
                     ></v-text-field>
                     <v-text-field
                         variant="outlined"
                         label="email"
+                        width="280px"
                         style="width: 70%;"
                     ></v-text-field>
                     <v-text-field
                         variant="outlined"
                         label="phone"
+                        width="280px"
                         style="width: 70%;"
                     ></v-text-field>
                     <v-text-field
                         variant="outlined"
                         label="phone owner"
+                        width="280px"
                         style="width: 70%;"
                     ></v-text-field>
                     
